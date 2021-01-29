ebb82375a9a78e53d9fd7ec1ee98edfe00a68454
zaid.khan <zaid.khan@tenpearls.com>
2021-01-29 10:47:24 +0500

    states

--- src/App.js
+++ src/App.js
@@ -4,14 +4,26 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+
+  state = {
+    persons: [
+      { name: "Elon Musk", age: 21 },
+      { name: "Bill Gates", age: 22 },
+      { name: "Mark Zingerburger", age: 23 }
+    ]
+    
+  }
+
+
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is really working</p>
-        <Person name="Elon Musk" age = "21"> <p>CEO SpaceX</p> </Person>
-        <Person name="Bill Gates" age = "22"> <p>Founder Microsoft</p> </Person>
-        <Person name="Mark Zingerburger" age = "23"> <p>CEO Facebook</p> </Person>
+        <button>Switch Name</button>
+        <Person name={this.state.persons[0].name} age = {this.state.persons[0].age}> <p>CEO SpaceX</p> </Person>
+        <Person name={this.state.persons[1].name} age = {this.state.persons[1].age}> <p>Founder Microsoft</p> </Person>
+        <Person name={this.state.persons[2].name} age = {this.state.persons[2].age}> <p>CEO Facebook</p> </Person>
       </div>
     );
   }
